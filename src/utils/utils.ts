5db2560217a581f968d06d88d04726dc1a6caf3b
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-15 17:47:33 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const CA = "WILL UPDATE SOON | CA: WILL UPDATE SOON";
+export const CA = "v9mbpEVYDwTqFDG5FboJYrorD7a6wcPrWF7f9r3pump";
 export const x = 'https://x.com/ClarqKentavious';
 export const tg = 'https://t.me/superniggasol';
 export const ig = 'https://www.instagram.com';
