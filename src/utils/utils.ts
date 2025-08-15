e89ee9660fa6f10cd605e4d1708298ce1b74a193
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-15 11:39:02 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,9 @@
-export const CA = "iWuZYrpVadwHNwrx8Ycf86KKj7sbyg7vL1gRCzebonk";
-export const x = 'https://x.com/supernigga';
-export const tg = 'https://t.me/supernigga';
-export const ig = 'https://www.instagram.com/supernigga/';
-export const tiktok = 'https://www.tiktok.com/@supernigga';
+export const CA = "WILL UPDATE SOON | CA: WILL UPDATE SOON";
+export const x = 'https://x.com/ClarqKentavious';
+export const tg = 'https://t.me/superniggasol';
+export const ig = 'https://www.instagram.com';
+export const tiktok = 'https://www.tiktok.com';
 
-export const buynow = `https://letsbonk.fun/token/${CA}`;
+export const buynow = `https://pump.fun/coin/${CA}`;
 export const dex = `https://dexscreener.com/solana/${CA}`;
 
